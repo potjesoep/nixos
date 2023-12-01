5267e5a3c6ec79224c2e8b2377929be7efa83f6e
Mia Winter Schat <nekonoor@proton.me>
2023-12-01 14:27:05 +0100

    feat: add nixpkgs as a flake input

--- flake.nix
+++ flake.nix
@@ -1,4 +1,10 @@
 {
+  description = "System configuration of cuddles";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
   outputs = { self, nixpkgs }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
