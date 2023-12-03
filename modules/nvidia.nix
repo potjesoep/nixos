69fbc36365d7cb718158850c05645fde6919ae58
Mia Winter Schat <nekonoor@proton.me>
2023-12-03 16:42:17 +0100

    feat: add nvidia configuration

---
+++ modules/nvidia.nix
@@ -0,0 +1,53 @@
+{ config, pkgs, ...}: {
+  # Enable OpenGL
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    powerManagement.enable = true;
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    # Do not disable this unless your GPU is unsupported or if you have a good reason to.
+    open = true;
+
+    # Enable the Nvidia settings menu,
+	# accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  services.xserver.screenSection = ''
+    Option "metamodes" "USB-C-0: 2560x1440_60 +3440+320 {rotation=left}, DP-0: 3440x1440_165 +0+1440, DP-2: 2560x1440_144 +880+0"
+  '';
+
+  # Add nvidia va-api driver for replay-sorcery to work
+  environment.systemPackages = with pkgs; [
+    nvidia-vaapi-driver
+  ];
+
+  # Set libva driver name for hardware accelerataed video encoding/decoding
+  environment.variables = {
+    LIBVA_DRIVER_NAME = "nvidia";
+    NVD_BACKEND = "direct";
+  };
+}
