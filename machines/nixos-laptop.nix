6bc5b05d6077899a78ab23269017a382b039b09c
Mia Winter Schat <nekonoor@proton.me>
2024-02-27 14:38:04 +0100

    feat: add hid-fanatecff, move stuff around

--- machines/nixos-laptop.nix
+++ machines/nixos-laptop.nix
@@ -3,9 +3,9 @@
 {
   networking.hostName = "nixos-laptop";
   environment.variables.VDPAU_DRIVER = "radeonsi";
-  services.xserver.xkb.variant = "workman";
   console.earlySetup = true;
   console.useXkbConfig = true;
+  services.xserver.xkb.variant = "workman";
   services.power-profiles-daemon.enable = false;
   services.tlp.enable = true;
 }
--- machines/nixos.nix
+++ machines/nixos.nix
@@ -1,30 +1,41 @@
-{ config, lib, ... }:
+{ pkgs, config, lib, ... }:
 
+let
+  fanatecff = config.boot.kernelPackages.callPackage ../pkgs/hid-fanatecff {};
+in
 {
   networking.hostName = "nixos";
-  boot.initrd.luks.devices."crypt_more".device = "/dev/disk/by-partlabel/part_more";
-  boot.initrd.luks.devices."crypt_quad".device = "/dev/disk/by-partlabel/part_quad";
-  boot.kernelModules = [ "it87" ];
-  boot.supportedFilesystems = [ "ntfs" ];
-  fileSystems."/mnt/huge" = {
-    device = "/dev/disk/by-label/tree_huge";
-    fsType = "btrfs";
+  boot = {
+    kernelModules = [ "it87" "hid-fanatec" ];
+    extraModulePackages = [ fanatecff ];
+    initrd.luks.devices."crypt_more".device = "/dev/disk/by-partlabel/part_more";
+    initrd.luks.devices."crypt_quad".device = "/dev/disk/by-partlabel/part_quad";
+    supportedFilesystems = [ "ntfs" ];
   };
-  fileSystems."/mnt/more" = {
-    device = "/dev/disk/by-label/tree_more";
-    fsType = "f2fs";
+  services.udev.packages = [ fanatecff ];
+  fileSystems = {
+    "/mnt/huge" = {
+      device = "/dev/disk/by-label/tree_huge";
+      fsType = "btrfs";
+    };
+    "/mnt/more" = {
+      device = "/dev/disk/by-label/tree_more";
+      fsType = "f2fs";
+    };
+    "/mnt/move" = {
+      device = "/dev/disk/by-label/tree_move";
+      fsType = "ntfs-3g";
+      options = [ "rw" "uid=cuddles" "nofail" ];
+    };
+    "/mnt/quad" = {
+      device = "/dev/disk/by-label/tree_quad";
+      fsType = "f2fs";
+    };
   };
-  fileSystems."/mnt/move" = {
-    device = "/dev/disk/by-label/tree_move";
-    fsType = "ntfs-3g";
-    options = [ "rw" "uid=cuddles" "nofail" ];
+  systemd.targets = {
+    sleep.enable = false;
+    suspend.enable = false;
+    hibernate.enable = false;
+    hybrid-sleep.enable = false;
   };
-  fileSystems."/mnt/quad" = {
-    device = "/dev/disk/by-label/tree_quad";
-    fsType = "f2fs";
-  };
-  systemd.targets.sleep.enable = false;
-  systemd.targets.suspend.enable = false;
-  systemd.targets.hibernate.enable = false;
-  systemd.targets.hybrid-sleep.enable = false;
 }
