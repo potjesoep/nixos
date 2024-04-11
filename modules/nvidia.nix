5391501ce6e045e90405fc57d7b3941621a82086
Mia Winter Schat <nekonoor@proton.me>
2024-04-12 01:54:37 +0200

    feat: switch to beta nvidia driver

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -40,7 +40,7 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.production;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 
   # Load nvidia_uvm again for some reason it stopped working
