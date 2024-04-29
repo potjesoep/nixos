e7d8277bcf6424261015c347cb66bd98ec2525be
Mia Winter Schat <nekonoor@proton.me>
2024-04-29 10:55:51 +0200

    fix: rename libinput settings

--- modules/input.nix
+++ modules/input.nix
@@ -8,7 +8,7 @@
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput = {
+  services.libinput = {
     enable = true;
     # disabling mouse acceleration
     mouse = {
