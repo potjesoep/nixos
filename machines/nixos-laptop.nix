2db3a58d7448433ce92d16a0a36c1f11b0b0f4da
Mia Winter Schat <nekonoor@proton.me>
2024-05-12 02:22:25 +0200

    feat: add libva driver and bluetooth codecs

--- machines/nixos-laptop.nix
+++ machines/nixos-laptop.nix
@@ -3,6 +3,7 @@
 {
   networking.hostName = "nixos-laptop";
   environment.variables.VDPAU_DRIVER = "radeonsi";
+  environment.variable.LIBVA_DRIVER_NAME = "radeonsi";
   console.earlySetup = true;
   console.useXkbConfig = true;
   services.xserver.xkb.variant = "workman";
