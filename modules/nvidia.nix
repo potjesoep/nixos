acd733abde5b0eed56c0202fc8a4e3a64dbc0055
Mia Winter Schat <nekonoor@proton.me>
2024-06-21 16:51:55 +0200

    feat: rename opengl settings names

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -2,10 +2,9 @@
 
 {
   # Enable OpenGL
-  hardware.opengl = {
+  hardware.graphics = {
     enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
+    enable32Bit = true;
     extraPackages = with pkgs; [
       vaapiVdpau
       libvdpau-va-gl
