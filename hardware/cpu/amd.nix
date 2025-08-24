d4d980be9b4479cd3d1afc5c13964e0f9c451b75
Mia Winter Schat <nekonoor@proton.me>
2025-08-25 01:23:52 +0200

    fix: sort amd.nix

--- hardware/cpu/amd.nix
+++ hardware/cpu/amd.nix
@@ -2,6 +2,6 @@
 
 {
   boot.kernelModules = [ "kvm-amd" "k10temp" ];
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
