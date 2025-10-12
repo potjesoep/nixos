9df8159582fd4580c89ac6aaced58819c27e4bbd
Soep <soep@tuta.io>
2025-10-12 20:22:14 +0200

    feat: remove ovmf section, added by default

--- system/virtualization.nix
+++ system/virtualization.nix
@@ -26,15 +26,6 @@
         package = pkgs.qemu_kvm;
         runAsRoot = true;
         swtpm.enable = true;
-        ovmf = {
-          enable = true;
-          packages = [ (pkgs.OVMFFull.override {
-            secureBoot = true;
-            tpmSupport = true;
-            tlsSupport = true;
-            httpSupport = true;
-          }) ];
-        };
       };
     };
   };
