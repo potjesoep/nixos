86f81c64f235c22cb3c30f0442b6767f2aa84dbc
Soep <soep@tuta.io>
2025-10-22 03:15:28 +0200

    feat: smb version 1.0 for mnt_move

--- machines/nixos-laptop.nix
+++ machines/nixos-laptop.nix
@@ -22,7 +22,7 @@
       fsType = "cifs";
       options = let
         automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-      in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100"];
+      in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100,vers=1.0"];
     };
   };
   networking.hostName = "nixos-laptop";
