f64158c7abe0bc5a62d569c577e1ec076f19c912
Soep <soep@tuta.io>
2026-06-10 00:15:06 +0200

    feat: add 8bitdo ultimate udev rules

--- apps/gaming.nix
+++ apps/gaming.nix
@@ -7,5 +7,15 @@
     mcaselector
   ];
 
+  services.udev.extraRules = ''
+    # 8BitDo Ultimate Bluetooth Controller's boot HID interface (seems to be shared by multiple 8BitDo devices)
+    # Change it only if lsusb prints different Product ID when device is in bootloader mode.
+    SUBSYSTEM=="hidraw", ATTRS{idProduct}=="3208", ATTRS{idVendor}=="2dc8", TAG+="uaccess"
+
+    # 8BitDo Ultimate Bluetooth Controller receiver's HID interface (exposed when the controller is not connected)
+    # When it is exposed, the upgrade tool can detect the receiver and automatically put in in bootloader mode.
+    SUBSYSTEM=="hidraw", ATTRS{idProduct}=="3109", ATTRS{idVendor}=="2dc8", TAG+="uaccess"
+  '';
+
   programs.steam.enable = true;
 }
